2a185739b722d032501e9a38ae011eae2370ae6d
Anurag Prasad <123822254+anuragpras@users.noreply.github.com>
2024-07-08 23:41:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,57 +11,51 @@ def main():
     # Projects section
     st.header('Projects')
 
-    # Table-like layout for projects
-    st.markdown('---')  # Horizontal line before the first block
-
     # First block of repositories
-    st.markdown('### Block 1')
-
     st.subheader('Image Caption Generator')
     st.markdown('[Image Caption Generator](https://github.com/anuragpras/image-description-deep-learning)')
     st.markdown('Image Caption Generator using Flickr dataset')
+    st.markdown('---')  # Horizontal line after the project
 
     st.subheader('Cafe Sales Analysis')
     st.markdown('[Cafe Sales Analysis](https://github.com/anuragpras/cafe-sales-analysis)')
     st.markdown('Detailed cafe sales analysis using Power BI and MySQL')
+    st.markdown('---')  # Horizontal line after the project
 
     st.subheader('Amazon India Sales Dashboard')
     st.markdown('[Amazon India Sales Dashboard](https://github.com/anuragpras/amazon-india-sales-dashboard-tableau)')
     st.markdown('Interactive Tableau dashboard analyzing Amazon India\'s sales data')
-
-    st.markdown('---')  # Horizontal line between blocks
-
-    # Second block of repositories
-    st.markdown('### Block 2')
+    st.markdown('---')  # Horizontal line after the project
 
     st.subheader('COVID-19 Tableau Dashboard')
     st.markdown('[COVID-19 Tableau Dashboard](https://github.com/anuragpras/covid19-tableau)')
     st.markdown('Analyzing COVID-19 data using Tableau')
+    st.markdown('---')  # Horizontal line after the project
 
     st.subheader('Exploratory Data Analysis')
     st.markdown('[Exploratory Data Analysis](https://github.com/anuragpras/exploratory-data-analysis)')
     st.markdown('E-Commerce Sales Insights using Python')
+    st.markdown('---')  # Horizontal line after the project
 
     st.subheader('Pizza Sales Analysis')
     st.markdown('[Pizza Sales Analysis](https://github.com/anuragpras/pizza-sales-analysis)')
     st.markdown('Analyzing pizza sales data using MySQL and Excel')
-
-    st.markdown('---')  # Horizontal line between blocks
-
-    # Third block of repositories
-    st.markdown('### Block 3')
+    st.markdown('---')  # Horizontal line after the project
 
     st.subheader('Random Poetry Generator')
     st.markdown('[Random Poetry Generator](https://github.com/anuragpras/not-by-gulzar)')
     st.markdown('Capture Gulzar\'s essence with playful verses')
+    st.markdown('---')  # Horizontal line after the project
 
     st.subheader('Pomodoro App')
     st.markdown('[Pomodoro App](https://github.com/anuragpras/pomodoro-app)')
     st.markdown('Simple Pomodoro timer app')
+    st.markdown('---')  # Horizontal line after the project
 
     st.subheader('Snake Game')
     st.markdown('[Snake Game](https://github.com/anuragpras/snake-game)')
     st.markdown('Classic snake game implemented in Python')
+    st.markdown('---')  # Horizontal line after the project
 
     # Contact section
     st.header('Contact')

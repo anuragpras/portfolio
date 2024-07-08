40616957fac8648dc29c1ed0aa4acba4e202d81e
Anurag Prasad <123822254+anuragpras@users.noreply.github.com>
2024-07-08 23:40:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,47 +11,57 @@ def main():
     # Projects section
     st.header('Projects')
 
-    # Two columns layout for projects
-    col1, col2 = st.columns(2)
+    # Table-like layout for projects
+    st.markdown('---')  # Horizontal line before the first block
 
     # First block of repositories
-    with col1:
-        st.subheader('Image Caption Generator')
-        st.markdown('[Image Caption Generator](https://github.com/anuragpras/image-description-deep-learning)')
-        st.markdown('Image Caption Generator using Flickr dataset')
+    st.markdown('### Block 1')
 
-        st.subheader('Cafe Sales Analysis')
-        st.markdown('[Cafe Sales Analysis](https://github.com/anuragpras/cafe-sales-analysis)')
-        st.markdown('Detailed cafe sales analysis using Power BI and MySQL')
+    st.subheader('Image Caption Generator')
+    st.markdown('[Image Caption Generator](https://github.com/anuragpras/image-description-deep-learning)')
+    st.markdown('Image Caption Generator using Flickr dataset')
 
-        st.subheader('Amazon India Sales Dashboard')
-        st.markdown('[Amazon India Sales Dashboard](https://github.com/anuragpras/amazon-india-sales-dashboard-tableau)')
-        st.markdown('Interactive Tableau dashboard analyzing Amazon India\'s sales data')
+    st.subheader('Cafe Sales Analysis')
+    st.markdown('[Cafe Sales Analysis](https://github.com/anuragpras/cafe-sales-analysis)')
+    st.markdown('Detailed cafe sales analysis using Power BI and MySQL')
 
-        st.subheader('COVID-19 Tableau Dashboard')
-        st.markdown('[COVID-19 Tableau Dashboard](https://github.com/anuragpras/covid19-tableau)')
-        st.markdown('Analyzing COVID-19 data using Tableau')
+    st.subheader('Amazon India Sales Dashboard')
+    st.markdown('[Amazon India Sales Dashboard](https://github.com/anuragpras/amazon-india-sales-dashboard-tableau)')
+    st.markdown('Interactive Tableau dashboard analyzing Amazon India\'s sales data')
 
-    with col2:
-        st.subheader('Exploratory Data Analysis')
-        st.markdown('[Exploratory Data Analysis](https://github.com/anuragpras/exploratory-data-analysis)')
-        st.markdown('E-Commerce Sales Insights using Python')
+    st.markdown('---')  # Horizontal line between blocks
 
-        st.subheader('Pizza Sales Analysis')
-        st.markdown('[Pizza Sales Analysis](https://github.com/anuragpras/pizza-sales-analysis)')
-        st.markdown('Analyzing pizza sales data using MySQL and Excel')
+    # Second block of repositories
+    st.markdown('### Block 2')
 
-        st.subheader('Random Poetry Generator')
-        st.markdown('[Random Poetry Generator](https://github.com/anuragpras/not-by-gulzar)')
-        st.markdown('Capture Gulzar\'s essence with playful verses')
+    st.subheader('COVID-19 Tableau Dashboard')
+    st.markdown('[COVID-19 Tableau Dashboard](https://github.com/anuragpras/covid19-tableau)')
+    st.markdown('Analyzing COVID-19 data using Tableau')
 
-        st.subheader('Pomodoro App')
-        st.markdown('[Pomodoro App](https://github.com/anuragpras/pomodoro-app)')
-        st.markdown('Simple Pomodoro timer app')
+    st.subheader('Exploratory Data Analysis')
+    st.markdown('[Exploratory Data Analysis](https://github.com/anuragpras/exploratory-data-analysis)')
+    st.markdown('E-Commerce Sales Insights using Python')
 
-        st.subheader('Snake Game')
-        st.markdown('[Snake Game](https://github.com/anuragpras/snake-game)')
-        st.markdown('Classic snake game implemented in Python')
+    st.subheader('Pizza Sales Analysis')
+    st.markdown('[Pizza Sales Analysis](https://github.com/anuragpras/pizza-sales-analysis)')
+    st.markdown('Analyzing pizza sales data using MySQL and Excel')
+
+    st.markdown('---')  # Horizontal line between blocks
+
+    # Third block of repositories
+    st.markdown('### Block 3')
+
+    st.subheader('Random Poetry Generator')
+    st.markdown('[Random Poetry Generator](https://github.com/anuragpras/not-by-gulzar)')
+    st.markdown('Capture Gulzar\'s essence with playful verses')
+
+    st.subheader('Pomodoro App')
+    st.markdown('[Pomodoro App](https://github.com/anuragpras/pomodoro-app)')
+    st.markdown('Simple Pomodoro timer app')
+
+    st.subheader('Snake Game')
+    st.markdown('[Snake Game](https://github.com/anuragpras/snake-game)')
+    st.markdown('Classic snake game implemented in Python')
 
     # Contact section
     st.header('Contact')
